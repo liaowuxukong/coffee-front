ff9b5dea941414a955e73a3993ee290c960f48e8
lucas <liaowuxukong@163.com>
2018-01-20 12:55:24 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <!-- 头部 -->
+    <v-header></v-header>
+    <!-- 主体切换 -->
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  import header from './components/header/header.vue';
+  export default {
+    components: {
+      'v-header': header
+    },
+    created() {
+    }
+  };
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+@import 'common/stylus/mixin.styl';
+
+.tab {
+  display: flex;
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  /* border: 1px solid rgba(7,17,27,0.1); */
+  border-1px(rgba(7, 17, 27, 0.1));
+}
+
+.tab .tab-item {
+  flex: 1;
+  text-align: center;
+}
+
+.tab .tab-item a {
+  display: block;
+  font-size: 14px;
+  color: rgb(77, 85, 93);
+}
+
+.tab .tab-item .active {
+  color: rgb(240, 20, 20);
+}
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="header">
+    <div class="content-wrapper">
+    </div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span>
+    </div>
+    <div class="background">
+      <img :src="banner.picUrl" alt="" class="" width="100%" height="100%">
+    </div>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    props: [],
+    data() {
+      return {
+        detailShow: false,
+        banner: ''
+      };
+    },
+    methods: {
+      showDetail() {
+        this.detailShow = true;
+      },
+      hideDetail() {
+        this.detailShow = false;
+      }
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+    },
+    mounted() {
+      this.$http.get('http://127.0.0.1:7001/bannerList')
+        .then(function (response) {
+        this.banner = response.body[0];
+      });
+    },
+    components: {
+    }
+  };
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "header.styl";
+</style>
